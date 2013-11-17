f4bd9b380300534d1f505e27e523afdefb713ef0
Yang <zhy0216@gmail.com>
2013-11-17 23:07:35 +0800

    add event binding

--- web/static/main.js
+++ web/static/main.js
@@ -1,9 +1,12 @@
 var $ = require("jquery");
 var _ = require("underscore");
 
+var SubChooser = require("page/leftsidebar").SubChooser;
+
 require("jquery.routes"); //https://github.com/thorsteinsson/jquery-routes
 
 ﻿$(function(){
-
+    var subchooser = new SubChooser();
+    subchooser.init()
 
 });
