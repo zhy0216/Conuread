fe8a77dc294914d474d437b6e64a45800a524db3
Yang <zhy0216@gmail.com>
2013-11-21 22:59:42 +0800

    make get feeds by feedsieid

--- web/controllers/frontend.py
+++ web/controllers/frontend.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, g, session
+from flask import (render_template, g, session,
+                   jsonify, request)
 
 from web.app import app
 from web.model import FeedSite
@@ -20,6 +21,11 @@ def read_site(feedsiteid=0):
                             feeds=feeds)
 
 
+@app.route("/api/feed", methods=["POST"])
+def feeds():
+    feedsiteid = request.form.get("feedsiteid") or None
+    if feedsiteid is None:
+        return jsonify(dict(rcode=404))
 
-
-
+    feeds = [feed.to_dict() for feed in FeedSite.get_feeds_by_id(feedsiteid)]
+    return jsonify(dict(rcode=200, feeds=feeds))
--- web/model/feed.py
+++ web/model/feed.py
@@ -63,6 +63,12 @@ class FeedSite(db.Document):
 
 
     @classmethod
+    def get_feeds_by_id(cls, feedsiteid, limit=15, page=1):
+        start = limit * page - limit
+        end = limit * page
+        return Feed.objects(feedsite=feedsiteid).ord_by("create_date")[start:end]
+
+    @classmethod
     def add_from_feed_url(cls,feed_url):
         site    = cls.get_from_feed_url(feed_url) or cls(feed_url=feed_url)
         if site.id:
